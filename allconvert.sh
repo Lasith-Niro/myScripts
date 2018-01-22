d2f07b66d0e1717c89cf503b16ff6917637c460c
Lasith-Niro <lasithniro.online@gmail.com>
2018-01-22 15:54:14 +0530

    Update allconvert.sh

--- allconvert.sh
+++ allconvert.sh
@@ -3,7 +3,7 @@
 # How to run:
 #		* ./allconvert.sh <path-string>
 
-
+$target = $1
 for i in "$1"; do
 	    ffmpeg -i "$i" -acodec libmp3lame -ab 160k -ar 44100 -ac 2 "$target${i%*.mp4}".mp3
     done
